18f033f19e3b927a81680793d4bd832eb5e46e12
Nishchay Kaushik <kaushiknishchay@gmail.com>
2018-06-20 14:50:27 +0530

    Add assets to JS

--- src/constants/assets.js
+++ src/constants/assets.js
@@ -3,5 +3,6 @@ export default {
     placeholderRestaurant: require('../../assets/images/placeholder-res.png'),
     placeholderFood: require('../../assets/images/placeholder-food.png'),
     pizza: require('../../assets/images/pizza.png'),
+    foodBg: require('../../assets/images/foodBg.png'),
   },
 };
