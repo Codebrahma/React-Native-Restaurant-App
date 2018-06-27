cded082c4eb5cc794eb98aad822b18ec7941a096
aayushis12 <aayushisinha84@gmail.com>
2018-06-27 18:32:58 +0530

    renamed files

--- web/routes.js
+++ web/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import CuisineGrid from './components/CuisineGrid';
 
 const Routes = () => (
   <BrowserRouter>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={LoginScreen} />
+    <Route path="/allCuisines" component={CuisineGrid} />
   </BrowserRouter>
 );
 
